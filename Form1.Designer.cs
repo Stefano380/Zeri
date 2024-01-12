4921f9935fa82be24b31578ae64fbca0c2c9a1b2
Stefano380 <127445006+Stefano380@users.noreply.github.com>
2024-01-12 10:54:59 +0100

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace Approsimazione_zeri
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnDisegna = new System.Windows.Forms.Button();
+            this.btnBisezione = new System.Windows.Forms.Button();
+            this.btnSecanti = new System.Windows.Forms.Button();
+            this.btnTangenti = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDisegna
+            // 
+            this.btnDisegna.Location = new System.Drawing.Point(21, 25);
+            this.btnDisegna.Name = "btnDisegna";
+            this.btnDisegna.Size = new System.Drawing.Size(75, 23);
+            this.btnDisegna.TabIndex = 0;
+            this.btnDisegna.Text = "disegna";
+            this.btnDisegna.UseVisualStyleBackColor = true;
+            this.btnDisegna.Click += new System.EventHandler(this.btnDisegna_Click);
+            // 
+            // btnBisezione
+            // 
+            this.btnBisezione.Location = new System.Drawing.Point(21, 129);
+            this.btnBisezione.Name = "btnBisezione";
+            this.btnBisezione.Size = new System.Drawing.Size(75, 23);
+            this.btnBisezione.TabIndex = 1;
+            this.btnBisezione.Text = "Bisezione";
+            this.btnBisezione.UseVisualStyleBackColor = true;
+            this.btnBisezione.Click += new System.EventHandler(this.btnBisezione_Click);
+            // 
+            // btnSecanti
+            // 
+            this.btnSecanti.Location = new System.Drawing.Point(21, 169);
+            this.btnSecanti.Name = "btnSecanti";
+            this.btnSecanti.Size = new System.Drawing.Size(75, 23);
+            this.btnSecanti.TabIndex = 2;
+            this.btnSecanti.Text = "Secanti";
+            this.btnSecanti.UseVisualStyleBackColor = true;
+            this.btnSecanti.Click += new System.EventHandler(this.btnSecanti_Click);
+            // 
+            // btnTangenti
+            // 
+            this.btnTangenti.Location = new System.Drawing.Point(21, 209);
+            this.btnTangenti.Name = "btnTangenti";
+            this.btnTangenti.Size = new System.Drawing.Size(75, 23);
+            this.btnTangenti.TabIndex = 3;
+            this.btnTangenti.Text = "Tangenti";
+            this.btnTangenti.UseVisualStyleBackColor = true;
+            this.btnTangenti.Click += new System.EventHandler(this.btnTangenti_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 4;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 85);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnTangenti);
+            this.Controls.Add(this.btnSecanti);
+            this.Controls.Add(this.btnBisezione);
+            this.Controls.Add(this.btnDisegna);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnDisegna;
+        private System.Windows.Forms.Button btnBisezione;
+        private System.Windows.Forms.Button btnSecanti;
+        private System.Windows.Forms.Button btnTangenti;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace Approsimazione_zeri
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static double Funzione(double x)
+        {
+            return x*x;
+        }
+
+        static double Derivata(double x)
+        {
+            double h = 0.00000001;
+            return (Funzione(x + h) - Funzione(x)) / h;
+        }
+
+        Point[] points;
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        public static double MetodoBisezione(double estremoa, double estremob)
+        {
+            double yA;
+            double yB;
+            double M = double.NaN;
+            double yM;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                M = (estremob + estremoa) / 2;
+                yM = Funzione(M);
+                yA = Funzione(estremoa);
+                yB = Funzione(estremob);
+
+                if (yM == 0)
+                    {
+                        return M;
+                    }
+                    else if (yA*yM < 0)
+                    {
+                        estremob = M;
+                    }
+                    else if (yB*yM < 0)
+                    {
+                        estremoa = M;
+                    }
+                    else if(yA * yM < yB * yM)
+                    {
+                        estremob= M;
+                    }
+                    else
+                    {
+                        estremoa = M;
+                    }
+            }
+            return M;
+        }
+
+        public static double MetodoSecanti(double estremoa, double estremob)
+        {
+            double yA;
+            double yB;
+            double ZeroRetta = 0;
+
+            double m;
+            double q;
+
+            for (int i = 0; i < 1000; i++)
+            {
+                yA = Funzione(estremoa);
+                yB = Funzione(estremob);
+
+                m = (yB - yA) / (estremob - estremoa);
+                q = yA - m * estremoa;
+
+                ZeroRetta = (-q) / m;
+                if (Funzione(ZeroRetta) > 0)
+                {
+                    estremob = ZeroRetta;
+                }
+                else
+                {
+                    estremoa = ZeroRetta;
+                }
+            }
+            return ZeroRetta;
+        }
+
+        public static double MetodoTangenti(double estremoa, double estremob)
+        {
+            double X0;
+
+            if((Derivata(estremoa) * estremoa - Funzione(estremoa)) / Derivata(estremoa)>estremoa && (Derivata(estremoa) * estremoa - Funzione(estremoa)) / Derivata(estremoa) < estremob)
+            {
+                X0 = (Derivata(estremoa) * estremoa - Funzione(estremoa)) / Derivata(estremoa);
+            }
+            else
+            {
+                X0 = (Derivata(estremob) * estremob - Funzione(estremob)) / Derivata(estremob);
+            }
+
+            for (int i = 0; i<100; i++)
+            {
+                X0 = (Derivata(X0) * X0 - Funzione(X0)) / Derivata(X0);
+            }
+            return X0;
+        }
+
+        private void btnDisegna_Click(object sender, EventArgs e)
+        {
+            int d = this.ClientSize.Width;
+
+            points = new Point[d];
+
+            for (int i = 0; i < points.Length; i++)
+            {
+                points[i] = new Point(i,(int)(-Funzione(i-d/2) + this.ClientSize.Height / 2));
+            }
+
+            Graphics a = this.CreateGraphics();
+
+            a.DrawCurve(new Pen(Color.Red), points);
+
+            a.DrawLine(new Pen(Color.Black), this.ClientSize.Width / 2, 0, this.ClientSize.Width / 2, this.ClientSize.Height);
+            a.DrawLine(new Pen(Color.Black), 0, this.ClientSize.Height / 2, this.ClientSize.Width, this.ClientSize.Height / 2);
+        }
+
+        private void btnBisezione_Click(object sender, EventArgs e)
+        {
+            double estremoa = double.Parse(textBox1.Text);
+            double estremob = double.Parse(textBox2.Text);
+
+            double soluzione = MetodoBisezione(estremoa, estremob);
+            MessageBox.Show(string.Format("{0}", soluzione));
+        }
+
+        private void btnSecanti_Click(object sender, EventArgs e)
+        {
+            double estremoa = double.Parse(textBox1.Text);
+            double estremob = double.Parse(textBox2.Text);
+
+            double soluzione = MetodoSecanti(estremoa, estremob);
+            MessageBox.Show(string.Format("{0}", soluzione));
+        }
+
+        private void btnTangenti_Click(object sender, EventArgs e)
+        {
+            double estremoa = double.Parse(textBox1.Text);
+            double estremob = double.Parse(textBox2.Text);
+
+            double soluzione = MetodoTangenti(estremoa, estremob);
+            MessageBox.Show(string.Format("{0}", soluzione));
+        }
+    }
+}
